4f2cf81561d02fff056cc3c48758613cb5bb616a
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-09 09:44:13 +0500

    made appsync api and dynamodb table

--- lib/v7-appsync_ddb_ds_s8-stack.ts
+++ lib/v7-appsync_ddb_ds_s8-stack.ts
@@ -1,9 +1,31 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as ddb from "@aws-cdk/aws-dynamodb";
+import * as appsync from "@aws-cdk/aws-appsync";
 
 export class V7AppsyncDdbDsS8Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // AppSync API
+    const api = new appsync.GraphqlApi(this, "step8", {
+      name: "step8API",
+      schema: appsync.Schema.fromAsset("graphql/schema.gql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+        },
+      },
+    });
+
+    const ddbTable = new ddb.Table(this, "ddbtable", {
+      tableName: "step8table",
+      partitionKey: {
+        name: "id",
+        type: ddb.AttributeType.STRING,
+      },
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
   }
 }
